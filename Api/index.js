3588107f8b787d0f3351fb577f22ecc89048dd1c
Rahul Sharma <122682371+RAHULSHARMA122002@users.noreply.github.com>
2025-04-28 19:17:52 +0530

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -1,33 +1,32 @@
+const express = require("express")
 
-const express = require('express');
-const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
-
-const auth = require('../Routes/authroute');
-const userRoute = require('../Routes/userRoute');
-const carRoute = require('../Routes/carroute');
-const bookingRoute = require('../Routes/BookingRoute');
-const reviewRoute = require('../Routes/ReviewRoute');
-const adminRoute = require('../Routes/AdminRoute');
-
 const app = express();
+const auth = require('./Routes/authroute');
+const { default: mongoose } = require("mongoose");
 
+const cors = require("cors")
 app.use(express.json());
 app.use(cors());
 
-// Define your routes
+
+
+
 app.use('/api/auth', auth);
-app.use('/api/user', userRoute);
-app.use('/api/cars', carRoute);
-app.use('/api/bookings', bookingRoute);
-app.use('/api/reviews', reviewRoute);
-app.use('/api/admin', adminRoute);
+app.use('/api/user', require('./Routes/userRoute'));
+app.use('/api/cars', require('./Routes/carroute'));
+app.use('/api/bookings', require('./Routes/BookingRoute'));
+app.use('/api/reviews', require('./Routes/ReviewRoute'));
+app.use('/api/admin', require('./Routes/AdminRoute'));
 
-// MongoDB connection
+// MongoDB connect + start server
+
 mongoose.connect("mongodb+srv://hare_krishna:Neelkanth1@cluster0.oijip3h.mongodb.net/CarRent?retryWrites=true&w=majority&appName=Cluster0")
-  .then(() => console.log('Connected to MongoDB!'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+.then(() => console.log('Connected to MongoDB!'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
-// Export the app for Vercel to handle serverless function requests
-module.exports = app;
+
+
+app.listen(5000,()=>{
+    console.log("hello backend");
+})
